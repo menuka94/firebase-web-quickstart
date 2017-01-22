ad058625603669768078f8ec4e81507433d00e3a
Menuka <menuka.14@cse.mrt.ac.lk>
2017-01-22 18:54:53 +0530

    Use Child Events

--- src/js/main.js
+++ src/js/main.js
@@ -16,14 +16,34 @@
 
 // get the element
     const preObject = document.getElementById('object');
+    const ulList = document.getElementById('list');
 
 // create references
     const dbRefObject = firebase.database().ref().child('object');
+    const dbRefList = dbRefObject.child('hobbies');
 
     // Sync object changes
     dbRefObject.on('value', snap => {
         preObject.innerText = JSON.stringify(snap.val(), null, 3);
     });
-    // snap == snapshot of data
+
+
+    // Sync list changes
+    dbRefList.on('child_added', snap => {
+        const li = document.createElement('li');
+        li.innerText = snap.val();
+        li.id = snap.key;
+        ulList.appendChild(li);
+    });
+
+    dbRefList.on('child_changed', snap => {
+        const liChanged = document.getElementById(snap.key);
+        liChanged.innerText = snap.val();
+    });
+
+    dbRefList.on('child_removed', snap => {
+        const liToRemove = document.getElementById(snap.key);
+        liToRemove.remove();
+    });
 
 })();
